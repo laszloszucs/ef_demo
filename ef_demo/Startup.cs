85c01ed23861447ea2b8a1328ee456301e9d74dd
László Szücs <laszlo.szucs@euroknows.com>
2020-02-21 21:19:41 +0100

    Update WeatherForecastController.cs, Program.cs, and Startup.cs

--- ef_demo/Startup.cs
+++ ef_demo/Startup.cs
@@ -1,14 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using VueCliMiddleware;
 
 namespace ef_demo
@@ -60,7 +54,6 @@ namespace ef_demo
                 {
                     spa.UseVueCli(npmScript: "serve");
                 }
-
             });
         }
     }
